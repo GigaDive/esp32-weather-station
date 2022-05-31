880756d22c4670a8c939087d7423a30226d9e87f
Fynf <65299553+Fynf@users.noreply.github.com>
2022-05-31 21:51:37 +0200

    🐛When changing datatypes, it is important to change them everywhere

--- indoor-station/src/services/WeekdayConverter.cpp
+++ indoor-station/src/services/WeekdayConverter.cpp
@@ -8,25 +8,25 @@ const char *intToGermanWeekday(char dayNumberChar)
     int dayNumber = dayNumberChar - '0';
     switch (dayNumber % 7)
     {
-    case '0':
+    case 0:
         return "Sonntag";
         break;
-    case '1':
+    case 1:
         return "Montag";
         break;
-    case '2':
+    case 2:
         return "Dienstag";
         break;
-    case '3':
+    case 3:
         return "Mittwoch";
         break;
-    case '4':
+    case 4:
         return "Donnerstag";
         break;
-    case '5':
+    case 5:
         return "Freitag";
         break;
-    case '6':
+    case 6:
         return "Samstag";
         break;
 
